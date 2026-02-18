afe0a6157bbb58b6714343ffc85693b4df8017e2
JasonP <jasonphillips97@gmail.com>
2026-02-17 19:24:03 -0500

    wrote intro prototype and user input prototype

--- Review of Procedural Programming/Review of Procedural Programming.cpp
+++ Review of Procedural Programming/Review of Procedural Programming.cpp
@@ -2,11 +2,70 @@
 //
 
 #include <iostream>
+#include <string>
+#include <cstdlib>
+#include < ctime>
+
+
+//Function Prototypes
+void DisplayIntro();
+int GetUserChoice();
+std::string choice_to_string(int choice);
+
+using namespace std;
+
+const int ROCK = 1;
+const int PAPER = 2;
+const int SCISSORS = 3;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    srand(static_cast<unsigned int>(time(0)));
+
+    DisplayIntro();
+
+    int UserChoice = GetUserChoice();
+
+
+    return 0;
 }
+
+//function definitions
+
+void DisplayIntro()
+{
+    cout << "welcome to Rock Paper Scissors" << endl;
+    cout << "1. Rock" << endl;
+    cout << "2. Paper" << endl;
+    cout << "3. Scissors" << endl;
+}
+
+int GetUserChoice() 
+{
+    int choice;
+    while (true) 
+    {
+        cout << "enter your choice. (1-3): ";
+        if (choice >= 1 && choice <= 3)
+        {
+            break;
+        }
+        else 
+        {
+            cout << "invalid input. Please choose 1, 2, or 3." << endl;
+        }
+    }
+    return choice;
+}
+
+ std::string choice_to_string(int choice) {
+    if (choice == ROCK) return "Rock";
+    if (choice == PAPER) return "Paper";
+    if (choice == SCISSORS) return "Scissors";
+    return "Unknown";
+}
+
+
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
